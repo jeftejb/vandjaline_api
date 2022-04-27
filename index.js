116d0a68a4bb0f7d0e7c7a265a4f730e1eb4d73a
jeftejb <jeftesambangojb@gmail.com>
2022-04-27 04:30:39 +0100

    my first commit

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ const cors = require("cors");
 dotenv.config();
 
 
+app.use(cors());
 app.use(express.json());
+/*
 app.use((req, res, next)=>{
-
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, OPTIONS, POST, DELETE, UPDATE");
     res.header("Access-Control-Allow-Headers", "*, Authorization");
@@ -28,7 +29,7 @@ app.use((req, res, next)=>{
     app.use(cors());
     next()
 });
-
+*/
 
 mongoose.connect(process.env.MONGO_URL /*'mongodb://127.0.0.1:27017/vandjaline_db'*/, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
     console.log("Conexao Criada com sucesso");
